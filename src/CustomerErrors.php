bab40e0c2e4c3f0f38082c246c0d5e216a9bf864
Julian Cole <julian@esvelte.com>
2015-06-24 13:29:35 +0100

    acceptable SOAP faults

---
+++ src/CustomerErrors.php
@@ -0,0 +1,22 @@
+<?php
+
+return  array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| List of SOAP errors which Airship retunrs that are customer facing
+	|--------------------------------------------------------------------------
+	*/
+
+    // Contact API error messages
+	'Email address is invalid',
+	'Mobile number is invalid',
+	'Post code is invalid',
+	'Post code is invalids',
+	'Invalid DOB',
+	'Invalid title'
+
+
+)
+
+?>
--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -5,7 +5,8 @@
 
 		public function __construct()
 		{
-			$this->errors = include 'Errors.php';
+			$this->errors = include 'Errors.php'; // error message config
+			$this->customer_soap_errors = include 'Errors.php'; // soap errors that are customer facing
 		}		
 
 		/**
@@ -24,16 +25,41 @@
 			if(isset($this->errors[$error[0]][$error[1]])){
 				$return->error_number =  $this->errors[$error[0]][$error[1]]['error_num'];
 				$return->error_message = $this->errors[$error[0]][$error[1]]['error_msg'];
+				$return->error_customer = $this->errors[$error[0]][$error[1]]['error_msg'];
 				if($soapfault !== false){// return a SOAP fault if we have one.
 					$return->soap_fault = $soapfault;
+					if($this->createCustomerSOAPerror()){ // check to see if soap fault is customer facing
+						$return->error_customer = $soapfault;
+					}
 				}
 			}else{ // no error message, so return a default
 				$return->error_number =  $this->errors[$error['default']][$error['default']]['error_num'];
 				$return->error_message = $this->errors[$error['default']][$error['default']]['error_msg'];
+				$return->error_customer = $this->errors[$error['default']][$error['default']]['error_customer'];
 			}
 			return $return;
 
 
+		}
+
+		/**
+		 * CREATE CUSTOMER SOAP ERROR
+		 *
+		 * @description If this is a SOAP error returned by airship, check if it's customer facing, and if not then show a default.
+		 * @param  String
+		 */
+
+
+		private function createCustomerSOAPerror($soapfault)
+		{
+			foreach($this->customer_soap_errors as $acceptable_error){
+
+				if (strpos(strtolower($soapfault),strtolower($acceptable_error)) !== false) {
+				    return true;
+				}
+
+			}
+			return false;
 		}
 		
 
