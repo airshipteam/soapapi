a706f3213f4da2431f5e70164b928f29554acc2a
Julian Cole <julian@esvelte.com>
2015-06-24 10:01:08 +0100

    namespacing

--- src/Airship.php
+++ src/Airship.php
@@ -1,32 +1,34 @@
-<?php namespace airshipwebservices\soapapi;
+<?php namespace airshipwebservices\soapapi {
 
-class Airship {
+	class Airship {
 
-	public $server;
-	public $username;
-	public $password;
+		public $server;
+		public $username;
+		public $password;
 
-	public $result;//some object that's being returned or false if there is an error
-	public $error;//holder for the error message
-
-
-	protected $soap_client;
+		public $result;//some object that's being returned or false if there is an error
+		public $error;//holder for the error message
 
 
-	public function __construct($server, $username, $password)
+		protected $soap_client;
+
+
+		public function __construct($server, $username, $password)
+			{
+				ini_set("soap.wsdl_cache_enabled", "0");
+
+				$this->server = $server;
+				$this->username = $username;
+				$this->password = $password;
+			}
+
+		protected function handleSoapErrors()
 		{
-			ini_set("soap.wsdl_cache_enabled", "0");
-
-			$this->server = $server;
-			$this->username = $username;
-			$this->password = $password;
+			
+			
 		}
 
-	protected function handleSoapErrors()
-	{
-		
-		
+	    
 	}
 
-    
 }
